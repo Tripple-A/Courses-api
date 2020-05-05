ede65fa4e6612abde7bc837dd62812d470962169
Tripple-A <biodun9@gmail.com>
2020-05-05 23:30:52 +0100

    added tests for the user model

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,5 +5,15 @@ module Api
       @users = User.all
       render json: { status: 'ok', message: 'Loaded Contacts', data: @users }, status: :ok
     end
+
+    def show
+      username = params[:id]
+      user = User.find_by(username: username)
+      render json: { status: 'ok', message: 'Loaded Contacts', data: user }, status: :ok
+    end
+
+    private
+    def user_params
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation test' do
+    it 'ensures username presence' do
+      user = User.new(username: '', password: '123').save
+      expect(user).to eq(false)
+    end
+
+    it 'ensures password presence' do
+      user = User.new(username: 'username', password: '').save
+      expect(user).to eq(false)
+    end
+  end
+
+  context 'creation and uniqueness test' do
+    user1 = User.new(username: 'barbey', password: '123').save
+    user2 = User.new(username: 'barbey', password: '123').save
+    
+    it 'creates a user successfully' do
+      expect(user1).to eq(true)
+    end
+
+    it 'ensures username uniqueness' do
+      expect(user2).to eq(false)
+    end
+
+  end
 end
