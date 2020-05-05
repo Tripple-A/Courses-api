97e24b4986a9090ce37b7a18656451496976020e
Tripple-A <biodun9@gmail.com>
2020-05-05 23:37:10 +0100

    started seeding the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-[{"courseId":14,"title":"Aws Developer Getting Started","shortDescription":"Developing high-performance web applications in the real world requires the use of a cloud provider, and Amazon Web Services is widely recognized as the leader in cloud technology. In this course, AWS Developer: Getting Started, you will learn how to develop applications that utilize many of...","url":"https://www.pluralsight.com/courses/aws-developer-getting-started","imgUrl":"https://pluralsight.imgix.net/course-images/aws-developer-getting-started-v1.jpg","location":null,"providerRatings":4.6,"extraDetails":null,"effort":null,"duration":4,"durationPeriod":"h","costUsd":null,"startDate":null,"provider":"Pluralsight","author":"Ryan Lewis","level":"Beginner","medium":"online","language":"English","academicProvider":null,"category":"aws","quzeCategory":"AWS","programType":"Online course","tags":"javascript,angular,typescript,zbrush,python,flask,pycharm,html,node.js,git,css,react.js,Front End,aws","quzeTags":"AWS","numUpvotes":0,"numRatings":0,"avgRatings":0}]
+ courses = [{"courseId":14,"title":"Aws Developer Getting Started","shortDescription":"Developing high-performance web applications in the real world requires the use of a cloud provider, and Amazon Web Services is widely recognized as the leader in cloud technology. In this course, AWS Developer: Getting Started, you will learn how to develop applications that utilize many of...","url":"https://www.pluralsight.com/courses/aws-developer-getting-started","imgUrl":"https://pluralsight.imgix.net/course-images/aws-developer-getting-started-v1.jpg","location":null,"providerRatings":4.6,"extraDetails":null,"effort":null,"duration":4,"durationPeriod":"h","costUsd":null,"startDate":null,"provider":"Pluralsight","author":"Ryan Lewis","level":"Beginner","medium":"online","language":"English","academicProvider":null,"category":"aws","quzeCategory":"AWS","programType":"Online course","tags":"javascript,angular,typescript,zbrush,python,flask,pycharm,html,node.js,git,css,react.js,Front End,aws","quzeTags":"AWS","numUpvotes":0,"numRatings":0,"avgRatings":0}]
+ courses.each do |course|
+    Course.create!(
+      title: course["title"],
+      shortDescription: course["shortDescription"],
+      url: course["url"]
+    )
+ end
