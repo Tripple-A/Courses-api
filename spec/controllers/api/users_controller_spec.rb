fd8382787b82bc528f1d18bf0f1205eed008abc4
Tripple-A <biodun9@gmail.com>
2020-05-07 22:23:34 +0100

    added tests for show action in user

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,9 +2,21 @@ require 'rails_helper'
 
 RSpec.describe Api::UsersController do
   describe "GET #index" do
-    before do
-      get :index
+    before {get :index}
+
+    it "returns http success" do
+      expect(response).to have_http_status(:ok)
     end
+
+    it "JSON body response contains expected recipe attributes" do
+      json_response = JSON.parse(response.body)
+      expect(json_response.keys).to match_array(["data", "message", "status"])
+    end
+  end
+
+  describe "GET #show" do
+  
+  before {get :show, :params => {"id" => 1}}
 
     it "returns http success" do
       expect(response).to have_http_status(:ok)
