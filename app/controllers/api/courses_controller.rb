2f1c51a5c7567fca156704dbb8f90bc4bd475f26
Tripple-A <biodun9@gmail.com>
2020-05-12 21:51:33 +0100

    added get a course to show courses controller

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -4,5 +4,15 @@ module Api
       @courses = Course.all
       render json: { status: 'ok', message: 'Loaded Courses', data: @courses }, status: :ok
     end
+
+    def show
+      course = Course.find(params[:id])
+      if course
+        render json: { status: 'ok', message: 'Loaded User', data: courses }, status: :ok
+      else
+        render json: { status: 500, message: 'Course not found' }, status: :not_found
+      end
+    end
+
   end
 end
