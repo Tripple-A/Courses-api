f7c5909828cf2fcac8dbcde8ef697c22bc5394a7
Tripple-A <biodun9@gmail.com>
2020-05-13 17:25:44 +0100

    made it impossible to save the same course to favorites

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,14 +1,20 @@
 module Api
   class FavoritesController < ApplicationController
     def create
-        fav = Favorite.new(fav_params)
-
-      if fav.save
-        render json: { status: 'SUCCESS' }, status: :ok
-      else
-        render json: { status: 'ERROR', data: fav.errors }, status: :not_found
-      end
+        user_id = User.find_by(username: params[:username]).id
+        course_id = params[:course_id]
+        if User.find(user_id).favorites.where(course_id:course_id).present?
+          render json: { data: 'Favorite already exists' }, status: :ok
+        else
+          fav = Favorite.new(user_id: user_id, course_id: course_id)
+          if fav.save
+            render json: { data: 'Favorite saved' }, status: :ok
+          else
+            render json: { data: 'There was a problem saving this course to your favorites' }, status: :not_found
+          end
+        end 
     end
+
     private
 
     def fav_params
