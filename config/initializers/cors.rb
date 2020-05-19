d486b756255144330d5f8ce81b4696d6992e54e8
Tripple-A <biodun9@gmail.com>
2020-05-19 13:02:16 +0100

    added live link to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -16,7 +16,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins 'https://learnbeta.netlify.app/'
+    origins 'https://learnbeta.netlify.app'
 
     resource '*',
              headers: :any,
